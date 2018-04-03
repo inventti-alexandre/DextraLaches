346f9c618fa8f5c2282513504a89b53d2c7e837a
Thiago Santos <thiago_aparecido@hotmail.com>
2018-04-03 02:40:07 -0300

    Add files via upload

---
+++ Lanches.Test/Lanches/UnitTestLanches.cs
@@ -0,0 +1,45 @@
+﻿using Lanches.Application.Interfaces;
+using Lanches.Application.Services;
+using Lanches.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lanches.Test.Lanches
+{
+    [TestClass]
+    public class UnitTestLanches
+    {
+        private readonly ILancheAppService _lancheAppService;
+
+        public UnitTestLanches(ILancheAppService lancheAppService)
+        {
+            this._lancheAppService = lancheAppService;
+        }
+
+        [TestMethod]
+        public void CalculaPrecoLanchePadrao()
+        {
+            //Arrange.
+            Lanche lanche = _lancheAppService.FindById(1);
+            decimal valorLanche = CalculaPrecoLanche(lanche);
+
+            //Act.
+            var preco = _lancheAppService.CalculaPrecoLanche(lanche);
+
+            //Assert.
+            Assert.IsTrue(preco == 0);
+
+        }
+
+        private decimal CalculaPrecoLanche(Lanche _lanche)
+        {
+            decimal somaIngredientes = 0;
+
+            foreach (var item in _lanche.Ingredientes)
+            {
+                somaIngredientes = somaIngredientes + item.Valor;
+            }
+
+            return somaIngredientes;
+        }
+    }
+}
---
+++ Lanches.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lanches.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lanches.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fc917e2e-008b-486d-8026-7075adeb282f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
